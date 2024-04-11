d9ea89fc333fd163b39afa3bb435d221e6ca8af1
João Darwin <joaodarwin.ip22@gmail.com>
2024-04-10 22:15:14 -0300

    feat: Adicionar parametros dimesionais ao app

--- App.tsx
+++ App.tsx
@@ -4,22 +4,30 @@ import {
   StyleSheet,
   Text
 } from 'react-native';
-
-function App(): React.JSX.Element {
-
-  return (
-    <SafeAreaView style={styles.App}>
-      <Text>Base Project</Text>
-    </SafeAreaView>
-  );
-}
+import params from './src/params';
 
 const styles = StyleSheet.create({
   App: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  welcome: {
+
+  },
+  instructions: {
+    
   }
 });
+
+function App(): React.JSX.Element {
+
+  return (
+    <SafeAreaView style={styles.App}>
+      <Text style={styles.welcome}>Iniciando o Mines!</Text>
+      <Text style={styles.instructions}>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+    </SafeAreaView>
+  );
+}
 
 export default App;
