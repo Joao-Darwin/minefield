bb0aa4d82cc52db63811b7d7382b534581642fb2
João Darwin <joaodarwin.ip22@gmail.com>
2024-04-19 23:58:07 -0300

    feat: Usar tabuleiro no App do projeto

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  Text
+  Text,
+  View
 } from 'react-native';
 import params from './src/params';
 import Field from './src/components/Field';
+import MineField from './src/components/MineField';
+import { createMinedBoard } from './src/gameLogic';
+import IField from './src/interfaces/IField';
 
 const styles = StyleSheet.create({
   App: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     alignItems: 'center'
   },
   welcome: {
@@ -18,23 +22,40 @@ const styles = StyleSheet.create({
   },
   instructions: {
 
+  },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
   }
 });
 
 function App(): React.JSX.Element {
 
+  const minesAmount = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  const createState = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+
+    return {
+      board: createMinedBoard(cols, rows, minesAmount())
+    }
+  }
+
+  const [boardState, setBoardState] = useState<IField[][]>(createState().board);
+
   return (
     <SafeAreaView style={styles.App}>
       <Text style={styles.welcome}>Iniciando o Mines!</Text>
       <Text style={styles.instructions}>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
-      <Field />
-      <Field opened={true} />
-      <Field opened={true} nearMines={1} />
-      <Field opened={true} nearMines={2} />
-      <Field opened={true} nearMines={6} />
-      <Field opened={true} mined={true} />
-      <Field opened={true} mined={true} exploded={true} />
-      <Field marked={true} />
+      <View style={styles.board}>
+        <MineField board={boardState} />
+      </View>
     </SafeAreaView>
   );
 }
