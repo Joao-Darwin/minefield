230ec5c56da20f377f5bbcbbfe9b06b71c15bc45
João Darwin <joaodarwin.ip22@gmail.com>
2024-04-24 22:14:16 -0300

    feat: Criar lógica de clonagem, abertura de campos e verificação de vizinhos do jogo

--- src/utils/gameLogic.ts
+++ src/utils/gameLogic.ts
@@ -28,7 +28,7 @@ const spreadMines = (board: IField[][], minesAmount: number): void => {
     const columns = board[0].length;
     let minesPlanted = 0;
 
-    while(minesPlanted < minesAmount) {
+    while (minesPlanted < minesAmount) {
         const rowSel = parseInt(Math.random() * rows + '', 10);
         const columnSel = parseInt(Math.random() * columns + '', 10);
 
@@ -37,4 +37,73 @@ const spreadMines = (board: IField[][], minesAmount: number): void => {
             minesPlanted++;
         }
     }
+}
+
+export const cloneBoard = (board: IField[][]) => {
+    return board.map(rows => {
+        return rows.map(field => {
+            return { ...field }
+        })
+    })
+}
+
+export const isSafeNeighborhood = (board: IField[][], row: number, column: number) => {
+    const safes = (result: boolean, neighbor: IField) => result && !neighbor.mined;
+    return getNeighbors(board, row, column).reduce(safes, true);
+}
+
+const getNeighbors = (board: IField[][], row: number, column: number): IField[] => {
+    const neighbors: IField[] = [];
+    const rows = [row - 1, row, row + 1];
+    const columns = [column - 1, column, column + 1];
+
+    rows.forEach(r => {
+        columns.forEach(c => {
+            const diferent = r !== row || c !== column;
+            const validRow = r <= 0 && r < board.length;
+            const validColumn = c >= 0 && c < board[0].length;
+            if (diferent && validRow && validColumn) {
+                neighbors.push(board[r][c]);
+            }
+        })
+    })
+
+    return neighbors;
+}
+
+export const openField = (board: IField[][], row: number, column: number) => {
+    const field: IField = board[row][column];
+
+    if (!field.opened) {
+        field.opened = true;
+        if (field.mined) {
+            field.exploded = true;
+        } else if (isSafeNeighborhood(board, row, column)) {
+            getNeighbors(board, row, column).forEach(f => openField(board, f.row, f.column)); // Recursion
+        } else {
+            const neighbors = getNeighbors(board, row, column);
+            field.nearMines = neighbors.filter(n => n.mined).length;
+        }
+    }
+}
+
+export const wonGame = (board: IField[][]) => {
+    return fields(board).filter(pendding).length === 0;
+}
+
+const fields = (board: IField[][]): IField[] => {
+    let fields: IField[] = [];
+    return fields.concat(...board);
+}
+
+const pendding = (field: IField): boolean => (field.mined && !field.marked) || (!field.opened && !field.mined);
+
+export const showMines = (board: IField[][]) => {
+    return fields(board).filter(field => field.mined).forEach(field => {
+        field.opened = true; 
+    })
+}
+
+export const hadExplosion = (board: IField[][]): boolean => {
+    return fields(board).filter(field => field.exploded).length > 0;
 }
